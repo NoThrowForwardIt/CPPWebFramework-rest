f54c6cce5baa378d21902cb3215718c2611084e3
unload <ialle.ironbits@gmail.com>
2019-03-25 19:28:11 -0300

    Delete config.h

--- config.h
+++
@@ -1,8 +0,0 @@
-//Config header file
-
-#define PSQL_HOST		"localhost"
-//#define PSQL_DB		"example_db"
-//#define PSQL_USER		"example_user"
-//#define PSQL_PASSWORD		"example_password"
-#define PSQL_PORT		5432
-//#define SERVER_CONF_DIR	"/home/example_user/cwf/server"	
